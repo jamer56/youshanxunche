7802df0b2a7f4f5a393b17f27ed5b589ff7e26d9
l8898 <l8898b@hotmail.com>
2024-05-17 23:45:47 +0800

    0.10.1 添加 权限管理AOP

---
+++ src/main/java/cc/llcon/youshanxunche/aop/SecurityAspect.java
@@ -0,0 +1,43 @@
+package cc.llcon.youshanxunche.aop;
+
+import cc.llcon.youshanxunche.mapper.UserMapper;
+import cc.llcon.youshanxunche.pojo.User;
+import cc.llcon.youshanxunche.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class SecurityAspect {
+
+    @Autowired
+    HttpServletRequest request;
+    @Autowired
+    UserMapper userMapper;
+
+    @Around("@annotation(cc.llcon.youshanxunche.anno.SecurityAuth)")
+    public Object doBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+        String jwt = request.getHeader("Authorization");
+        Claims claims = JwtUtils.parseJWT(jwt);
+        String uid = (String) claims.get("id");
+        Integer permission = (Integer) claims.get("permission");
+
+
+        if (permission ==1){
+            User user = userMapper.getById(uid);
+            user.setPassword(null);
+            user.setSalt(null);
+
+            throw new RuntimeException("权限不足 使用者"+user);
+//            return Result.error("权限不足");
+        }
+        return joinPoint.proceed();
+    }
+}
---
+++ src/main/java/cc/llcon/youshanxunche/controller/LogController.java
@@ -0,0 +1,21 @@
+package cc.llcon.youshanxunche.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class LogController {
+
+/**
+ * 获取操作日志
+ */
+
+/**
+ * 获取查询日志
+ */
+
+/**
+ * 获取错误日志
+ */
+}
