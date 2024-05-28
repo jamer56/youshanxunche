5a3d2e583e4ba2c1a3e3038ebb5d94da60e60cdb
l8898 <l8898b@hotmail.com>
2024-05-29 06:54:11 +0800

    0.14.11 暫時使用原始方法保存key

--- src/main/java/cc/llcon/youshanxunche/utils/JwtUtils.java
+++ src/main/java/cc/llcon/youshanxunche/utils/JwtUtils.java
@@ -3,12 +3,15 @@ package cc.llcon.youshanxunche.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 import java.util.Map;
 
+@Slf4j
 public class JwtUtils {
-    private static final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+//    private static final String SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+    private static final String SECRET_KEY = "8711d18c0fb74e0f84f3e443722c7a5d";
     private static final Long EXPIRE = 3600000L;
 
 
@@ -16,6 +19,8 @@ public class JwtUtils {
         if (SECRET_KEY == null) {
             throw new RuntimeException("SECRET_KEY 為空",new RuntimeException("JWT錯誤"));
         }
+        log.info("是否讀取到環境變量:{}",System.getenv("JWT_SECRET_KEY")==null);
+
         String jwt = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .setClaims(claims)
